6a7d2c5343b21157ba389fc1a66155b824786b91
irregularuntenable <irregularuntenablepassport@gmail.com>
2024-01-06 23:04:29 -0600

    changed pg connection

--- src/login/newPool.ts
+++ src/login/newPool.ts
@@ -1,10 +1,14 @@
-import pg from 'pg';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new pg.Pool({
-  connectionString: process.env.POSTGRES,
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
 });
 
 export default pool;
