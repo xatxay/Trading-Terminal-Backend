9c6205f3276f4af5ae5e8ce4364240e3da6a6a8e
irregularuntenable <irregularuntenablepassport@gmail.com>
2024-01-07 09:39:06 -0600

    added https support

--- src/newScrape/server.ts
+++ src/newScrape/server.ts
@@ -2,8 +2,11 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import endpointRouter from './endpointRouter.js';
 import rateLimit from 'express-rate-limit';
-// import { sendAccountInfoRequest } from './utils.js';
-// import session from 'express-session';
+import https from 'https';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const apiLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
@@ -12,7 +15,7 @@ const apiLimiter = rateLimit({
 
 const startServer = (): void => {
   const app: Express = express();
-  const PORT = Number(process.env.PORT);
+  const PORT = Number(process.env.PORT) || 443;
 
   // const corsOptions = {
   //   // TODO: allow for all traffic (done)
@@ -20,24 +23,27 @@ const startServer = (): void => {
   //   credentials: true,
   // };
 
-  // app.use(
-  //   session({
-  //     secret: 'thisismysecretkey',
-  //     resave: false,
-  //     saveUninitialized: true,
-  //     cookie: { httpOnly: true, secure: true },
-  //   }),
-  // );
   app.use(cors());
   app.use(express.json());
   app.use(endpointRouter);
   app.use(apiLimiter);
 
-  // sendAccountInfoRequest(app);
+  const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+  const certificatePath = process.env.CERTIFICATE_PATH;
+  const privateKey = fs.readFileSync(privateKeyPath, 'utf-8');
+  const certificate = fs.readFileSync(certificatePath, 'utf-8');
 
-  app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  const credentials = { key: privateKey, cert: certificate };
+
+  const httosServer = https.createServer(credentials, app);
+
+  httosServer.listen(PORT, () => {
+    console.log(`Server is running on https://localhost:${PORT}`);
   });
+
+  // app.listen(PORT, () => {
+  //   console.log(`http://localhost:${PORT}`);
+  // });
 };
 
 export default startServer;
